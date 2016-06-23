bec386379eae7f9ed8054b950d4f27844ec76c6a
Long Do <long.do@audiencemedia.com>
2016-06-23 11:04:22 +0700

    Add methods to Body

--- src/Request/Body.php
+++ src/Request/Body.php
@@ -1,6 +1,57 @@
 <?php namespace Titan\Http\Request;
 
+use Titan\Common\Content\ContentAwareTrait;
+use Titan\Common\Content\Parser\ParserAwareTrait;
+use Titan\Common\Exception\InvalidArgumentException;
+use Titan\Common\StreamAwareTrait;
+
 class Body implements BodyInterface
 {
+    use ContentAwareTrait, StreamAwareTrait, ParserAwareTrait;
 
+    /**
+     * @var string
+     */
+    private $parsedContent;
+
+    /**
+     * @inheritDoc
+     */
+    public function __toString()
+    {
+        $content = '';
+        if ($this->getStream() !== null) {
+            $content = $this->getStream();
+        } elseif ($this->getContent() !== null) {
+            $content = $this->getContent();
+        }
+
+        return (string) $content;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getParsedContent()
+    {
+        if ($this->parsedContent !== null) {
+            return $this->parsedContent;
+        }
+
+        if ($this->getParser() === null) {
+            throw new InvalidArgumentException("There is no parsers available.");
+        }
+
+        return $this->getParser()->parse($this->getContent());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setParsedContent($parsedContent)
+    {
+        $this->parsedContent = $parsedContent;
+
+        return $this;
+    }
 }
--- src/Request/BodyInterface.php
+++ src/Request/BodyInterface.php
@@ -1,6 +1,11 @@
 <?php namespace Titan\Http\Request;
 
-interface BodyInterface
+use Titan\Common\Content\ContentAwareInterface;
+use Titan\Common\Content\Parser\ParserAwareInterface;
+use Titan\Common\Exception\InvalidArgumentException;
+use Titan\Common\Stream\StreamAwareInterface;
+
+interface BodyInterface extends ContentAwareInterface, StreamAwareInterface, ParserAwareInterface
 {
     /**
      * Return the content of body as string
@@ -8,4 +13,20 @@ interface BodyInterface
      * @return string
      */
     public function __toString();
+
+    /**
+     * Parse and return parsed content
+     *
+     * @return mixed
+     * @throws InvalidArgumentException
+     */
+    public function getParsedContent();
+
+    /**
+     * Set parsed content
+     *
+     * @param string $parsedContent
+     * @return self
+     */
+    public function setParsedContent($parsedContent);
 }
